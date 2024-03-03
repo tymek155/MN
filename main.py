430b214e0cdc7fd082010c7c472b439e68153d93
Tymek155 <tymek.maca@outlook.com>
2024-03-03 14:11:58 +0100

    :)

---
+++ main.py
@@ -0,0 +1,42 @@
+def file_read(nazwa, tab1, tab2):
+    with open(nazwa, "r") as odczyt:
+        ilosc = int(odczyt.readline())
+
+        for i in range(ilosc):
+            wart1, wart2 = map(float, odczyt.readline().split())
+            tab1.append(wart1)
+            tab2.append(wart2)
+
+        return ilosc
+
+
+def interpolacja(ilosc, xi, fxi, punkt):
+    wynik = 0
+
+    for i in range(ilosc):
+        licznik = 1
+        mianownik = 1
+        for j in range(ilosc):
+            if i != j:
+                licznik *=(punkt-xi[j])
+                mianownik*=(xi[i]-xi[j])
+
+        wynik+=fxi[i]*(licznik/mianownik)
+
+    return wynik
+
+
+def main():
+    xi = []
+    fxi = []
+    ilosc_wezlow = file_read("MN.txt", xi, fxi)
+    print(f"Liczba węzłów wynosi: {ilosc_wezlow}")
+    print(f"Węzły interpolacji: {xi}")
+    print(f"Wartości w węzłach: {fxi}")
+    punkt = int(input("Podaj punkt w ktorym chcesz policzyc wartosc wielamianu: "))
+    print(f"Podany punkt {punkt}")
+    print(f"Wartosc wielomianu Lagrange'a w podanym punkcie wynosi {interpolacja(ilosc_wezlow, xi, fxi, punkt)}")
+
+
+if __name__ == '__main__':
+    main()
